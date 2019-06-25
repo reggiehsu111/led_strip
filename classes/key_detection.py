0ef8fb279c7d0a95abc37cd4e0cd61e0ebd67cee
reggiehsu111 <reggiehsu111@gmail.com>
2019-06-25 03:32:23 -0900

    w,t changeable

--- classes/key_detection.py
+++ classes/key_detection.py
@@ -5,15 +5,23 @@ class  key_detector():
 	def __init__(self,w,t):
 		self.w = w
 		self.t = t
+		self.w_delta = 0.2
+		self.t_delta = 0.001
 		self.key_listener= Listener(on_press=self.on_press)
 
 	def on_press(self,key):
 		if key == Key.up:
-			self.w += 1
-			print("\nself.w: ", self.w)
+			self.w += self.w_delta
+			print("\nw: ", self.w)
 		elif key == Key.down:
-			self.w -= 1
-			print("\nself.w: ", self.w)
+			self.w -= self.w_delta
+			print("\nw: ", self.w)
+		elif key == Key.right:
+			self.t += self.t_delta
+			print("\nt: ", self.t )
+		elif key == Key.left:
+			self.t -= self.t_delta
+			print("\nt: ", self.t )
 
 	def start(self):
 		self.key_listener.start()
