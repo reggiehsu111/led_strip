3d440bfcaa099276851c28ca774f39e1f0e45212
reggiehsu111 <reggiehsu111@gmail.com>
2019-06-24 18:48:07 -0900

    need to fix keyboard input

--- run_led.py
+++ run_led.py
@@ -1,8 +1,8 @@
 import time
-import keyboard
+
 
 # Running Pattern
-def Run(led_strip):
+def Run(led_strip,w,t):
 	# led_strip.pixels.fill((255, 0, 0))
 	# led_strip.pixels.show()
 	# time.sleep(1)
@@ -12,8 +12,7 @@ def Run(led_strip):
 	# led_strip.pixels.fill((0, 0, 255))
 	# led_strip.pixels.show()
 	# time.sleep(1)
-	w = 100
-	t= 0.01
+	
 	led_strip.display_led(w,t)
 	time.sleep(t)
 
